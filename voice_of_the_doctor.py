aee2e6e1975eaab1708f95ee2f47cfb808aa7836
subhashbishnoi <me22b2044@iiitdm.ac.in>
2025-03-12 21:58:39 +0530

    Initial commit

---
+++ voice_of_the_doctor.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+#Step1a: Setup Text to Speech–TTS–model with gTTS
+import os
+from gtts import gTTS
+
+import subprocess
+import platform
+
+def text_to_speech_with_gtts(input_text, output_filepath):
+    language="en"
+
+    audioobj= gTTS(
+        text=input_text,
+        lang=language,
+        slow=False
+    )
+    audioobj.save(output_filepath)
+    os_name = platform.system()
+    try:
+        if os_name == "Darwin":  # macOS
+            subprocess.run(['afplay', output_filepath])
+        elif os_name == "Windows":  # Windows
+            subprocess.run(['powershell', '-c', f'(New-Object Media.SoundPlayer "{output_filepath}").PlaySync();'])
+        elif os_name == "Linux":  # Linux
+            subprocess.run(['aplay', output_filepath])  # Alternative: use 'mpg123' or 'ffplay'
+        else:
+            raise OSError("Unsupported operating system")
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+
+input_text="Hi this is subhash from Rajasthan, autoplay"
+text_to_speech_with_gtts(input_text=input_text, output_filepath="gtts_testing.mp3")
