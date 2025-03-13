73fe59772bd2aabd9651a5c8fd8d0795913628fd
Subhash <149309073+Subhashbisnoi@users.noreply.github.com>
2025-03-13 22:53:20 +0530

    Update brain_of_the_doctor.py

--- brain_of_the_doctor.py
+++ brain_of_the_doctor.py
@@ -15,8 +15,7 @@ def encode_image(image_path):
     return base64.b64encode(image_file.read()).decode('utf-8')
 
 from groq import Groq
-#query="Is there acene on my face?, if yes how to remove it"
-#model="llama-3.2-90b-vision-preview"
+
 def analyze_image_with_query(query, model, encoded_image):
     client=Groq()  
     messages=[
